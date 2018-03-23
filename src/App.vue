a102c9314564091adb068498ecd839ff0f7c7c12
disgraceful <noolic97@gmail.com>
2018-03-23 19:54:17 +0200

    Create header template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <v-app>
+    <mh-header></mh-header>
     <mh-player></mh-player>
   </v-app>
 </template>
 
 <script>
 import Player from "./components/player/Player"
+import Header from "./components/header/Header"
 
   export default {
     components:{
-      'mh-player':Player
+      'mh-player':Player,
+      'mh-header':Header
     }
   }
 </script>
