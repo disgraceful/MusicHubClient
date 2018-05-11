afeb9ce6dce10f339231e4b9eee9faf9cc5ac398
disgraceful <noolic97@gmail.com>
2018-05-11 21:05:58 +0300

    Player controls: next, prev, add to playlist, finished Player template

---
+++ src/components/player/AddToPlaylistDialog.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card>
+        <v-card-title class="subheading">Add to playlist</v-card-title>
+        <v-list subheader>
+            <v-list-tile v-for="playlist in playlists" :key="playlist">
+                <v-list-tile-action>
+                    <v-icon >add</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title v-html="playlist"></v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+        <v-card-actions>
+            <v-text-field hide-details single-line v-model="playlistSearch"></v-text-field>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                playlists:[
+                    "Favorites","playlist1","playlist2"
+                ],
+                playlistSearch:""
+            }
+        },
+        props: ['dialog']
+    }
+</script>
--- src/components/player/Player.vue
+++ src/components/player/Player.vue
@@ -1,43 +1,183 @@
 <template>
-
     <div id="player">
         <v-card tile>
-            <v-progress-linear height="3" :value="50" class="my-0"></v-progress-linear>
-            <v-list>
-                <v-list-tile>
-                    <v-list-tile-action>
-                        <v-btn icon>
-                            <v-icon>fast_rewind</v-icon>
-                        </v-btn>
-                    </v-list-tile-action>
-                    <v-list-tile-action :class="{ 'mx-5': $vuetify.breakpoint.mdAndUp }">
-                        <v-btn icon>
-                            <v-icon>pause</v-icon>
-                        </v-btn>
-                    </v-list-tile-action>
-                    <v-list-tile-action :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }">
-                        <v-btn icon>
-                            <v-icon>fast_forward</v-icon>
-                        </v-btn>
-                    </v-list-tile-action>
-                   
-                    <v-list-tile-content>
-                        <v-list-tile-title>The Walker</v-list-tile-title>
-                        <v-list-tile-sub-title>Fitz & The Trantrums</v-list-tile-sub-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
+            <v-container pa-0 style="height:75px">
+                <v-progress-linear height="3" :value="50" class="my-0"></v-progress-linear>
+                <v-layout row>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon @click="prev()">
+                                <v-icon>fast_rewind</v-icon>
+                            </v-btn>
+                        </v-container>
+                    </v-flex>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon @click="play(songs[currentIndex])">
+                                <v-icon large>{{playing?'pause':'play_arrow'}}</v-icon>
+                            </v-btn>
+                        </v-container>
+                    </v-flex>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon @click="next()">
+                                <v-icon>fast_forward</v-icon>
+                            </v-btn>
+                        </v-container>
+                    </v-flex>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height style="height:75px">
+                            <img src="http://via.placeholder.com/55x55" width="55px" height="55px" />
+                        </v-container>
+                    </v-flex>
+                    <v-card-title>
+                        <div class="text-xs-left" style="width:80px">
+                            <div class="text--black">{{currentSong.name}}</div>
+                            <div class="grey--text">{{currentSong.author}}</div>
+                        </div>
+                    </v-card-title>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon>
+                                <v-icon>favorite_border</v-icon>
+                            </v-btn>
+                        </v-container>
+                    </v-flex>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-dialog v-model="playlistDialog" max-width="290">
+                                <v-btn icon slot="activator">
+                                    <v-icon>add</v-icon>
+                                </v-btn>
+                                <mh-add-dialog :dialog="playlistDialog"></mh-add-dialog>
+                            </v-dialog>
+                        </v-container>
+                    </v-flex>
+                    <v-spacer></v-spacer>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon>
+                                <v-icon>shuffle</v-icon>
+                            </v-btn>
+
+                        </v-container>
+                    </v-flex>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon>
+                                <v-icon>repeat</v-icon>
+                            </v-btn>
+                        </v-container>
+                    </v-flex>
+                    <v-flex shrink>
+                        <v-container pa-0 fill-height>
+                            <v-btn icon>
+                                <v-icon>volume_up</v-icon>
+                            </v-btn>
+                        </v-container>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+
+
         </v-card>
     </div>
 </template>
 
 <script>
-    export default {};
+import AddToPlaylist from './AddToPlaylistDialog'
+    export default {
+        data() {
+            return {
+                playlistDialog: false,
+                songs: [{
+                        name: "Kill Yourself",
+                        author: "Suicide Boys",
+                        duration: "3:01",
+                        url: '../../../music/JohnCenaMyTimeIsNow.mp3',
+                        image: "http://via.placeholder.com/350x150"
+                    },
+                    {
+                        name: "Dead Inside",
+                        author: "Arch Enemy",
+                        duration: "4:02",
+                        url: '../../../music/TaylorSwiftEyesOpen.mp3',
+                        image: "http://via.placeholder.com/350x150"
+                    },
+                    {
+                        name: "Retard Whore",
+                        author: "Demented We Go",
+                        duration: "2:03",
+                        url: '../../../music/EminemBeautifulPain.mp3',
+                        image: "http://via.placeholder.com/350x150"
+                    }
+                ],
+                playing: false,
+                currentSong: {
+                    name: " ",
+                    author: " ",
+                    duration: "",
+                    url: ''
+
+                },
+                currentAudio: null,
+                currentIndex: 0,
+            }
+        },
+        methods: {
+            play(song) {
+                console.log(this.currentSong);
+                if (!this.currentSong.url) {
+                    this.currentSong = song;
+                    this.currentAudio = new Audio(this.currentSong.url);
+                }
+                console.log(this.currentSong.url);
+                if (!this.playing) {
+                    this.currentAudio.play();
+                    this.playing = true;
+                } else {
+                    this.currentAudio.pause();
+                    this.playing = false;
+                }
+            },
+            next() {
+                if (this.currentIndex < this.songs.length - 1) {
+                    this.currentIndex++;
+                    this.currentAudio.pause();
+                    this.currentSong = this.songs[this.currentIndex];
+                    this.currentAudio = new Audio(this.currentSong.url);
+                    this.playing = true;
+                    this.currentAudio.play();
+                }
+            },
+            prev() {
+                if (this.currentIndex <= 0) {
+                    return;
+                }
+                this.currentIndex--;
+                this.currentAudio.pause();
+                this.currentSong = this.songs[this.currentIndex];
+                this.currentAudio = new Audio(this.currentSong.url);
+                this.playing = true;
+                this.currentAudio.play();
+            },
+            fav() {
+
+            },
+            add() {
+
+            }
+
+        },components:{
+            'mh-add-dialog':AddToPlaylist
+        }
+    }
 </script>
 
 <style scoped>
     #player {
         position: fixed;
+
         bottom: 0;
         width: 70%;
         margin-left: 15%;
