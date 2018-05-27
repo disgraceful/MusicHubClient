00538171aafd244044b5e86db0481dd72096a6ab
disgraceful <noolic97@gmail.com>
2018-05-27 13:02:12 +0300

    Album Previwe for rest

--- src/components/shared/AlbumPreview.vue
+++ src/components/shared/AlbumPreview.vue
@@ -1,19 +1,25 @@
 <template>
-    <v-card>
-        <v-card-media :src="album.imgPath" height="150px">
-        </v-card-media>
+    <v-card id="album-card" flat>
+        <v-container>
+            <v-card-media :src="album.imgPath" height="150px">
+                <v-btn relative fab left small color="pink" class="fab-icon">
+                    <v-icon medium>favorite_border</v-icon>
+                </v-btn>
+                <v-btn relative fab left small color="yellow" class="fab-icon">
+                    <v-icon medium>play_arrow</v-icon>
+                </v-btn>
+            </v-card-media>
+        </v-container>
         <v-card-title>
             <v-flex pa-0>
                 <div class="text-xs-left">
-                    <router-link :to="{name:'ArtistGeneral',params:{id:album.authorId}}">
-                        <div class="subheading" style="font-weight:bold">{{album.name}}</div>
+                    <router-link :to="{name:'ArtistGeneral',params:{id:album.authorId}}" tag="div" class="subheading album-text" style="font-weight:bold">
+                        {{album.name}}
                     </router-link>
-                    <router-link :to="{name:'Album',params:{id:album.id}}">
-                        <div class="text--black">{{album.authorName}}</div>
+                    <router-link :to="{name:'Album',params:{id:album.id}}" tag="div" class="album-text">
+                        {{album.authorName}}
                     </router-link>
-                    <router-link :to="{name:'Genre',params:{id:album.genreId}}">
-                        <div class="grey--text">{{album.recordDate}}</div>
-                    </router-link>
+                    <div class="grey--text">{{album.recordDate}}</div>
                 </div>
             </v-flex>
         </v-card-title>
@@ -26,5 +32,38 @@
     }
 </script>
 
-<style>
+<style scoped>
+    .album-text {
+        color: black !important;
+        cursor: pointer;
+    }
+
+    .album-text:hover {
+        color: red !important;
+    }
+
+    .container {
+        padding: 5px;
+    }
+
+    #album-card {
+        padding: 5px;
+        border: 2px solid transparent;
+    }
+
+    #album-card:hover {
+        border: 2px solid;
+        border-color: yellow;
+    }
+
+    .fab-icon {
+        top: 50px;
+        display: none;
+    }
+
+    #album-card:hover .fab-icon {
+        display: flex;
+    }
+
+
 </style>
